af35cab2088d7cc5f7014ec810e529cca7df83d5
mequam <blue9ja@gmail.com>
2026-03-31 22:10:42 -0400

    added support for java lsp

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -10,6 +10,11 @@ return {
 		})
       vim.lsp.enable({'pyright'})
       vim.lsp.enable({'ts_ls'})
+
+      --vim.lsp.config("java_language_server",{ cmd={"java-lang-server"} })
+      --vim.lsp.enable("java_language_server")
+      vim.lsp.enable("jdtls")
+
 		vim.lsp.config("*", { capabilities = vim.lsp.protocol.make_client_capabilities() })
 
 
@@ -23,7 +28,7 @@ return {
        vim.keymap.set("n", "<leader>s", "<cmd>lua vim.lsp.buf.signature_help()<CR>" )
        vim.keymap.set("n", "<leader>lr", "<cmd>lua vim.lsp.buf.rename()<CR>" )
        vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>" )
-       vim.keymap.set("n", "<leader>f", "<cmd>lua vim.diagnostic.open_float()<CR>" )
+       vim.keymap.set("n", "<leader>d", "<cmd>lua vim.diagnostic.open_float()<CR>" )
        vim.keymap.set("n", "[d", '<cmd>lua vim.diagnostic.goto_prev({ border = "single" })<CR>' )
        vim.keymap.set("n", "]d", '<cmd>lua vim.diagnostic.goto_next({ border = "single" })<CR>' )
        vim.keymap.set("n", "<leader>q", "<cmd>lua vim.diagnostic.setloclist()<CR>" )
